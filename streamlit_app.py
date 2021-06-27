a44438f38c56ec47e5a0402e2bb37b4ee1201a4e
seongw <faceon@gmail.com>
2021-06-27 20:22:55 +0900

    First commit

---
+++ streamlit_app.py
@@ -0,0 +1,34 @@
+from fastai.text.all import *
+import streamlit as st
+import _pickle as cPickle
+
+# Streamlit launches a web app
+st.markdown("# Readability Test")
+
+class HowReadable:
+    def __init__(self, model_file):
+        # Load model file into a learner
+        self.learner = load_learner(model_file)
+
+        # Load a text input form
+        self.text = self.get_text()
+
+        # Show readability score
+        self.show_pred()
+
+    def get_text(self):
+        return st.text_input(
+            label='Input text to rate readability score',
+            max_chars=300,
+        )
+    
+    def show_pred(self):
+        if self.text:
+            pred = self.learner.predict(self.text)[0][0]
+            st.write(f'Readability score: {pred}')
+
+
+if __name__ == '__main__':
+    model_file = 'model_lstm.pkl'  # TODO: to be parameterized
+    how_readable = HowReadable(model_file)
+    
